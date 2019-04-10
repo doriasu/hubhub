b1f0240369677eee9e1c80fb6e234f2b6fe368f5
doriasu <44011386+doriasu@users.noreply.github.com>
2019-04-11 00:19:35 +0900

    upload file

---
+++ final.py
@@ -0,0 +1,515 @@
+eki={"shibuya":0,"omotesandou":1,"gaienmae":2,"aoyamaicchoume":3,"akasakamituki":4,"tameikesannnou":5,"toranomon":6,"sinbashi":7,"ginza":8,"kyobashi":9,"nihonbashi":10,"mitsukoshimae":11,"kanda":12,"suehirocho":13,"uenokoukouji":14,"ueno":15,"inanicho":16,"tawaramachi":17,"asakusa":18,
+"ogikubo":20,"minamiasagaya":21,"sinnkoennji":22,"higashikoennji":23,"sinnnnakano":24,"honanncho":25,"nakanohujimicho":26,"nakanoshinnbashi":27,"nakanosakagami":28,"nishisinnjuku":29,"sinjuku":30,"shinnjukusanchome":31,"shinjukugoennmae":32,"yotuyasannchome":33,"yotuya":34,"kasumigaseki":35,"tokyo":36,"otemachi":37,"awajicho":38,"ochanomizu":39,"hongosanchome":40,"korakuen":41,"irakadani":42,"sinnotuka":43,"ikebukuro":44,
+"nakameguro":50,"ebisu":51,"hiroo":52,"ropponngi":53,"kamitanicho":54,"hibiya":55,"higasiginnza":56,"tukiji":57,"hacchobori":58,"kayabacho":59,"ninngyocho":60,"kodennbacho":61,"akihabara":62,"iriya":63,"sannnowa":64,"minamisennju":65,"kitasennju":66,
+"nakano":70,"ochiai":71,"takadanobaba":72,"waseda":73,"kagurazaka":74,"iidabashi":75,"kudanshita":76,"takebasi":77,"monnzennnakamachi":78,"mokuba":79,"touyoucho":80,"nannsacho":81,"nishikasai":82,"kasai":83,"urayasu":84,"nannkoutoku":85,"koutoku":86,"myoudenn":87,"haraginakayama":88,"nisihunabasi":89,
+"yoyogiuehara":90,"yoyogikoenn":91,"meijijinnguumae":92,"nogizaka":93,"akasaka":94,"hutaebasimae":95,"yusima":96,"nedu":97,"senndagi":98,"nisinippori":99,"matiya":100,"ayase":101,"kitaayase":102,
+"wakoshi":110,"tikatetuseizou":111,"tikatetuakatuka":112,"heiwadai":113,"hikawadai":114,"kotakekouhara":115,"senngawa":116,"kanamemati":117,"higasiikebukuro":118,"gokokuji":119,"edogawabasi":120,"ichigaya":121,"marimati":122,"sakuradamonn":123,"ginnzaicchome":124,"sinnhucho":125,"tukisima":126,"toyosu":127,"tatuomi":128,"sinnkiba":129,"zojigadani":130,"nisiwaseda":131,"higasisinnjuku":132,"kitasanndou":133,
+"hannzomonn":140,"jinnbocho":141,"suitenngumae":142,"seisumihakuga":143,"sumiyosi":144,"kinnsicho":145,"osigami":146,
+"meguro":150,"hakkinndai":151,"hakkinnkourinn":152,"azabujubann":153,"ropponngiicchome":154,"toudaimae":155,"honnkomagome":156,"komagome":157,"nisigahara":158,"ouji":159,"oujikamiya":160,"simo":161,"akabanegannbuti":162
+}
+rosennzu='''163
+0 1 2
+1 0 2
+1 2 2
+2 1 2
+2 3 1
+3 2 1
+3 4 2
+4 3 2
+4 5 2
+5 4 2
+5 6 2
+6 5 2
+6 7 2
+7 6 2
+7 8 2
+8 7 2
+8 9 2
+9 8 2
+9 10 1
+10 9 1
+10 11 2
+11 10 2
+11 12 2
+12 11 2
+12 13 1
+13 12 1
+13 14 2
+14 13 2
+14 15 1
+15 14 1
+15 16 2
+16 15 2
+16 17 2
+17 16 2
+17 18 1
+18 17 1
+20 21 2
+21 20 2
+21 22 2
+22 21 2
+23 22 2
+22 23 2
+23 24 2
+24 23 2
+25 26 2
+26 25 2
+26 27 2
+27 26 2
+24 28 2
+28 24 2
+27 28 2
+28 27 2
+28 29 2
+29 28 2
+29 30 2
+30 29 2
+30 31 2
+31 30 2
+31 32 1
+32 31 1
+32 33 2
+33 32 2
+33 34 2
+34 33 2
+34 4 3
+4 34 3
+5 35 2
+35 5 2
+35 8 3
+8 35 3
+8 36 2
+36 8 2
+36 37 2
+37 36 2
+37 38 2
+38 37 2
+38 39 1
+39 38 1
+39 40 2
+40 39 2
+40 41 2
+41 40 2
+41 42 3
+42 41 3
+42 43 2
+43 42 2
+43 44 3
+44 43 3
+50 51 2
+51 50 2
+51 52 3
+52 51 3
+52 53 3
+53 52 3
+53 54 4
+54 53 4
+54 35 2
+35 54 2
+35 55 2
+55 35 2
+55 8 2
+8 55 2
+8 56 1
+56 8 1
+56 57 2
+57 56 2
+57 58 2
+58 57 2
+58 59 2
+59 58 2
+59 60 2
+60 59 2
+60 61 2
+61 60 2
+61 62 2
+62 61 2
+62 14 1
+14 62 1
+15 63 2
+63 15 2
+63 64 2
+64 63 2
+64 65 2
+65 64 2
+65 66 3
+66 65 3
+70 71 3
+71 70 3
+71 72 3
+72 71 3
+72 73 3
+73 72 3
+73 74 2
+74 73 2
+74 75 2
+75 74 2
+75 76 2
+76 75 2
+76 77 2
+77 76 2
+77 37 2
+37 77 2
+37 10 2
+10 37 2
+10 59 1
+59 10 1
+59 78 3
+78 59 3
+78 79 2
+79 78 2
+79 80 1
+80 79 1
+80 81 3
+81 80 3
+81 82 3
+82 81 3
+82 83 2
+83 82 2
+83 84 2
+84 83 2
+84 85 2
+85 84 2
+85 86 2
+86 85 2
+86 87 2
+87 86 2
+87 88 3
+88 87 3
+88 89 3
+89 88 3
+90 91 2
+91 90 2
+91 92 2
+92 91 2
+92 1 1
+1 92 1
+1 93 3
+93 1 3
+93 94 2
+94 93 2
+94 5 2
+5 94 2
+55 95 2
+95 55 2
+95 37 2
+37 95 2
+38 96 2
+96 38 2
+96 97 2
+97 96 2
+97 98 2
+98 97 2
+98 99 2
+99 98 2
+99 100 2
+100 99 2
+100 66 4
+66 100 4
+66 101 3
+101 66 3
+101 102 4
+102 101 4
+110 111 3
+111 110 3
+111 112 2
+112 111 2
+112 113 3
+113 112 3
+113 114 2
+114 113 2
+114 115 2
+115 114 2
+115 116 2
+116 115 2
+116 117 2
+117 116 2
+117 44 3
+44 117 3
+44 118 2
+118 44 2
+118 119 2
+119 118 2
+119 120 3
+120 119 3
+120 75 2
+75 120 2
+75 121 3
+121 75 3
+121 122 2
+122 121 2
+122 4 1
+4 122 1
+4 123 2
+123 4 2
+123 55 2
+55 123 2
+55 124 2
+124 55 2
+124 125 1
+125 124 1
+125 126 3
+126 125 3
+126 127 2
+127 126 2
+127 128 2
+128 127 2
+128 129 3
+129 128 3
+44 130 3
+130 44 3
+130 131 3
+131 130 3
+131 132 2
+132 131 2
+132 31 2
+31 132 2
+31 133 3
+133 31 3
+133 92 2
+92 133 2
+92 0 2
+0 92 2
+4 140 2
+140 4 2
+140 76 2
+76 140 2
+76 141 1
+141 76 1
+141 37 3
+37 141 3
+37 11 2
+11 37 2
+11 142 3
+142 11 3
+142 143 3
+143 142 3
+143 144 3
+144 143 3
+144 145 2
+145 144 2
+145 146 2
+146 145 2
+150 151 2
+151 150 2
+151 152 2
+152 151 2
+152 153 3
+153 152 3
+153 154 2
+154 153 2
+154 5 2
+5 154 2
+34 121 2
+121 34 2
+75 41 2
+41 75 2
+41 155 3
+155 41 3
+155 156 2
+156 155 2
+156 157 2
+157 156 2
+157 158 2
+158 157 2
+158 159 2
+159 158 2
+159 160 3
+160 159 3
+160 161 2
+161 160 2
+161 162 2
+162 161 2'''
+
+
+
+
+import heapq
+class Dijkstra:
+    def __init__(self,g):
+        lines = g.split("\n")
+        self.size = int(lines[0]) 
+        self.list = [[] for i in range(self.size)]
+        for s in lines[1:]:
+            d1,d2,w = s.split(" ")
+            self.connect(int(d1),int(d2),int(w)) 
+    def connect(self,x,y,w):
+        self.list[x].append([y,w]) 
+    def dump(self):
+        for i in range(len(self.list)):
+            for u in self.list[i]:
+                print(i," ",u[0]," ",u[1])
+    def trav(self,s):
+        d = [float('inf') for i in range(self.size)] 
+        d[s] = 0
+        fmap = [None for i in range(self.size)]
+        pq = [] 
+        u = s   
+        while True:
+            for n in self.list[u]: 
+                v,weight = n 
+                if d[v] > d[u]+weight:                             
+                    d[v] = d[u]+weight
+                    heapq.heappush(pq,[d[v],v]) 
+                    fmap[v] = u                                    
+            if len(pq) == 0: 
+                break
+            weight,u = heapq.heappop(pq) 
+        return [d,fmap] 
+
+def tansaku(d,fmap,to,saishuu,saisho):
+    if to==saisho:
+        for key,value in eki.items():
+            if value==to:
+                print(key)
+        
+    if to==saishuu:
+        print("かかる時間は",d[saisho],"分")
+    else:
+        for key,value in eki.items():
+            if value==fmap[to]:
+                print(key)
+        to=fmap[to]
+        tansaku(d,fmap,to,saishuu,saisho)
+
+def keiro(d,fmap,to,saishuu,saisho):
+    if to==saisho:
+        for key,value in eki.items():
+            if value==to:
+                print(key)
+        
+    if to==saishuu:
+        a=1
+    else:
+        for key,value in eki.items():
+            if value==fmap[to]:
+                print(key)
+        to=fmap[to]
+        keiro(d,fmap,to,saishuu,saisho)
+
+def tansakukai(d,fmap,to,saishuu,saisho,list):
+    if to==saisho:
+        for key,value in eki.items():
+            if value==to:
+                list.append(key)
+        
+    if to==saishuu:
+        a=1
+        
+    else:
+        for key,value in eki.items():
+            if value==fmap[to]:
+                list.append(key)
+        to=fmap[to]
+        tansakukai(d,fmap,to,saishuu,saisho,list)
+    
+def rosen(n):
+    if n>=0 and n<=18:
+        return "ginzasen"
+    if n>=20 and n<=44:
+        return "marunouchisen"
+    if n>=50 and n<66:
+        return "hibiyasen"
+    if n>=70 and n<=89:
+        return "tozaisen"
+    if n>=90 and n<=102:
+        return "chiyodasen"
+    if n>=110 and n<=133:
+        return "yurakuchosen,hukutoshinsen"
+    if n>=140 and n<=146:
+        return "hanzomonsen"
+    if n>=150 and n<=162:
+        return "nanbokusen"
+    
+    
+    
+def test():
+    ninzuu=int(input("何人で行きますか？"))
+    if ninzuu==1:
+        print("出発駅を入力してください（shibuyaなど）")
+        shuppatu=str(input())
+        print("到着駅を入力してください")
+        touchaku=str(input())
+
+    
+        dk = Dijkstra(rosennzu)    
+        d,fmap = dk.trav(eki[touchaku]) 
+        tansaku(d,fmap,eki[shuppatu],eki[touchaku],eki[shuppatu])
+    if ninzuu==2:
+        print("一人目の出発駅を入力して下さい。")
+        shuppatuhitorime=str(input())
+        print("二人目の出発駅を入力して下さい。")
+        shuppatuhutarime=str(input())
+        print("目的地を入力してください")
+        mokutekiti=str(input())
+            
+        print("目的地に早くつきたいですか？それとも早く会いたいですか？前者なら１、後者なら２を選択")
+        N=int(input())
+        dk = Dijkstra(rosennzu)      
+        if N==1:
+            listA=[]
+            listB=[]
+            dA,fmapA=dk.trav(eki[mokutekiti])
+            tansakukai(dA,fmapA,eki[shuppatuhitorime],eki[mokutekiti],eki[shuppatuhitorime],listA)
+            tansakukai(dA,fmapA,eki[shuppatuhutarime],eki[mokutekiti],eki[shuppatuhutarime],listB)
+            handan=False
+            gouryu=0
+            for i in range(len(listA)):
+                for j in range(len(listB)):
+                    if listA[i]==listB[j]:
+                        print(listA[i],"で合流すべきです。")
+                        gouryu=i
+                        handan=True
+                        break
+                if handan:
+                    break
+            print("一人目の目的地までの経路です")
+            for i in range(len(listA)):
+                print(listA[i])
+            print("二人目の目的地までの経路です")
+            for i in range(len(listB)):
+                print(listB[i])
+            
+
+        if N==2:
+            dA,fmapA=dk.trav(eki[shuppatuhitorime]) 
+            dB,fmapB=dk.trav(eki[shuppatuhutarime])
+            min=100
+            eeki="huhu"
+            ll=0
+            for i in range(0,163):
+                if dA[i]==None or dB[i]==None:
+                    continue
+                if dA[i]+dB[i]<min:
+                    min=dA[i]+dB[i]
+                    for key,value in eki.items():
+                        if value==i:
+                            eeki=key
+                            ll=i
+            dA,fmapA=dk.trav(eki[eeki]) 
+            dB,fmapB=dk.trav(eki[eeki])
+            print(eeki,"で合流すべきです。")
+            print("一人目のまちあわせ駅までの経路です")
+            keiro(dA,fmapA,eki[shuppatuhitorime],ll,eki[shuppatuhitorime])
+            print("二人目の待ち合わせ駅までの経路です")
+            keiro(dB,fmapB,eki[shuppatuhutarime],ll,eki[shuppatuhutarime])
+            dC,fmapC=dk.trav(eki[mokutekiti])
+            print("待ち合わせ駅から目的地までの経路です")
+            keiro(dC,fmapC,eki[eeki],eki[mokutekiti],eki[eeki])
+
+
+
+                
+
+            
+            
+
+            
+            
+            
+    
+test()
+
+
+    
+
